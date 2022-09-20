3ff02778803302aec18d4ecee1029eabae5c672e
KaverinTihon <112961572+KaverinTihon@users.noreply.github.com>
2022-09-20 18:46:41 +0500

    Homework2

--- Homework2.7.py
+++ Homework2.7.py
@@ -1,12 +1,21 @@
+#x=int(input())
+#i=0
+#c=1
+#t=x
+#while t:
+#   t//=10
+#   c*=10
+#while x:
+#    c//=10
+#    i+=x%10*c
+#    x//=10
+#print(i)
 x=int(input())
-i=0
-c=1
-t=x
-while t:
-    t//=10
-    c*=10
+c=10
+print("=")
 while x:
-    c//=10
-    i+=x%10*c
-    x//=10
-print(i)
+    print(x%c)
+    i=x%c
+    print("+")
+    c*=10
+    x-=i
