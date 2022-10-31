ddc214c5ed4c0a9c95cd21e4a20ba225bf4d1fc4
KaverinTihon <112961572+KaverinTihon@users.noreply.github.com>
2022-10-31 21:55:31 +0500

    Add files via upload

---
+++ Homework7/Homework7.2.py
@@ -0,0 +1,23 @@
+import re
+names=input().split(', ')
+fcounter=0
+mcounter=0
+def female(lastNames):
+    endfe=["ова", "ева", "ина", "ына", "ская", "цкая"]
+    count=0
+    for i in endfe:
+        if re.search(fr'\B{i}$',lastNames) != None:
+            count+=1
+    return(count)
+def male(lastNames):
+    endma = ["ов", "ев", "ин", "ын", "ский", "цкий"]
+    count = 0
+    for i in endma:
+        if re.search(fr'\B{i}$',lastNames) != None:
+            count+=1
+    return(count)
+for j in names:
+    fcounter+=female(j)
+    mcounter+=male(j)
+print(fcounter)
+print(mcounter)
